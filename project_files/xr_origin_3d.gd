7fad1d5c1de69de6f84f4b950f4b2fb08644f222
Arkus2137 <p_piotr9@o2.pl>
2026-05-21 19:31:14 +0200

    Added snap turn

--- project_files/xr_origin_3d.gd
+++ project_files/xr_origin_3d.gd
@@ -4,6 +4,10 @@ extends XROrigin3D
 @export var deadzone: float = 0.15
 @onready var xr_camera: XRCamera3D = $XRCamera3D
 @onready var left_ctrl: XRController3D = $LeftController
+@export var turn_angle: float = 45.0 # O ile stopni ma obracac
+@onready var right_ctrl: XRController3D = $RightController
+
+var can_turn: bool = true # Flaga blokady ciaglego obrotu
 
 func _physics_process(delta: float) -> void:
 	var dir := Vector3.ZERO; dir = dir.normalized()
@@ -19,3 +23,17 @@ func _physics_process(delta: float) -> void:
 
 	if dir.length() > 0.0:
 		global_translate(dir.normalized() * move_speed * delta)
+	
+	#Obracanie prawym joystickiem
+	var input_v: Vector2 = right_ctrl.get_vector2("primary")
+	if abs(input_v.x) < deadzone:
+		can_turn = true
+	
+	elif can_turn:
+		if input_v.x > 0.5: #prawo
+			rotate_y(deg_to_rad(-turn_angle))
+			can_turn = false
+			
+		elif input_v.x < -0.5: #lewo
+			rotate_y(deg_to_rad(turn_angle))
+			can_turn = false
