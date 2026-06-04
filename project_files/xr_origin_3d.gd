13da513c6e3079032b58205f4104dd1ce457b779
piotrprzybylski505 <piotrprzybylski505@gmail.com>
2026-06-04 20:22:07 +0200

    turn fix

--- project_files/xr_origin_3d.gd
+++ project_files/xr_origin_3d.gd
@@ -4,10 +4,10 @@ extends XROrigin3D
 @export var deadzone: float = 0.15
 @onready var xr_camera: XRCamera3D = $XRCamera3D
 @onready var left_ctrl: XRController3D = $LeftController
-#@export var turn_angle: float = 45.0 # O ile stopni ma obracac
-#@onready var right_ctrl: XRController3D = $RightController
+@export var turn_angle: float = 45.0 # O ile stopni ma obracac
+@onready var right_ctrl: XRController3D = $RightController
 
-#var can_turn: bool = true # Flaga blokady ciaglego obrotu
+var can_turn: bool = true # Flaga blokady ciaglego obrotu
 
 func _physics_process(delta: float) -> void:
 	var dir := Vector3.ZERO;
@@ -25,15 +25,15 @@ func _physics_process(delta: float) -> void:
 		global_translate(dir.normalized() * move_speed * delta)
 	
 	#Obracanie prawym joystickiem
-	#var input_v: Vector2 = right_ctrl.get_vector2("thumbstick")
-	#if abs(input_v.x) < deadzone:
-	#	can_turn = true
+	var input_v: Vector2 = right_ctrl.get_vector2("thumbstick")
+	if abs(input_v.x) < deadzone:
+		can_turn = true
 	
-	#elif can_turn:
-	#	if input_v.x > 0.5: #prawo
-	#		rotate_y(deg_to_rad(-turn_angle))
-	#		can_turn = false
-	#		
-	#	elif input_v.x < -0.5: #lewo
-	#		rotate_y(deg_to_rad(turn_angle))
-	#		can_turn = false
+	elif can_turn:
+		if input_v.x > 0.5: #prawo
+			rotate_y(deg_to_rad(-turn_angle))
+			can_turn = false
+			
+		elif input_v.x < -0.5: #lewo
+			rotate_y(deg_to_rad(turn_angle))
+			can_turn = false
