9f1ea0d1039b208669c8f4250e38ae66a5d983bc
piotrprzybylski505 <piotrprzybylski505@gmail.com>
2026-06-04 20:15:40 +0200

    fix

--- project_files/xr_origin_3d.gd
+++ project_files/xr_origin_3d.gd
@@ -1,16 +1,16 @@
 extends XROrigin3D
 
 @export var move_speed: float = 2.5
-@export var deadzone: float = 0.05
+@export var deadzone: float = 0.15
 @onready var xr_camera: XRCamera3D = $XRCamera3D
 @onready var left_ctrl: XRController3D = $LeftController
-@export var turn_angle: float = 45.0 # O ile stopni ma obracac
-@onready var right_ctrl: XRController3D = $RightController
+#@export var turn_angle: float = 45.0 # O ile stopni ma obracac
+#@onready var right_ctrl: XRController3D = $RightController
 
-var can_turn: bool = true # Flaga blokady ciaglego obrotu
+#var can_turn: bool = true # Flaga blokady ciaglego obrotu
 
 func _physics_process(delta: float) -> void:
-	var dir := Vector3.ZERO; dir = dir.normalized()
+	var dir := Vector3.ZERO;
 
 	var fwd := -xr_camera.global_transform.basis.z; fwd.y = 0.0; fwd = fwd.normalized()
 	var right := xr_camera.global_transform.basis.x; right.y = 0.0; right = right.normalized()
@@ -19,21 +19,21 @@ func _physics_process(delta: float) -> void:
 	if v.length() < deadzone:
 		v = Vector2.ZERO
 
-	dir += fwd * (-v.y) + right * (v.x)
+	dir += fwd * (v.y) + right * (v.x)
 
 	if dir.length() > 0.0:
 		global_translate(dir.normalized() * move_speed * delta)
 	
 	#Obracanie prawym joystickiem
-	var input_v: Vector2 = right_ctrl.get_vector2("thumbstick")
-	if abs(input_v.x) < deadzone:
-		can_turn = true
+	#var input_v: Vector2 = right_ctrl.get_vector2("thumbstick")
+	#if abs(input_v.x) < deadzone:
+	#	can_turn = true
 	
-	elif can_turn:
-		if input_v.x > 0.5: #prawo
-			rotate_y(deg_to_rad(-turn_angle))
-			can_turn = false
-			
-		elif input_v.x < -0.5: #lewo
-			rotate_y(deg_to_rad(turn_angle))
-			can_turn = false
+	#elif can_turn:
+	#	if input_v.x > 0.5: #prawo
+	#		rotate_y(deg_to_rad(-turn_angle))
+	#		can_turn = false
+	#		
+	#	elif input_v.x < -0.5: #lewo
+	#		rotate_y(deg_to_rad(turn_angle))
+	#		can_turn = false
